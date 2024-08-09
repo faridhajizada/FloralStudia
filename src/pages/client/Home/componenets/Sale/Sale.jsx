505ac90147be8d80c65664cbe86828720cc62587
ilaha-jaf <ilash.jafarova@gmail.com>
2024-08-09 17:55:45 +0400

    Sale

---
+++ src/pages/client/Home/componenets/Sale/Sale.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Reklam.scss';
+
+function Sale() {
+  return (
+    <div className='reklam'>
+      <div className='reklam-container'>
+        <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/sl3-1050x700.jpg" alt="flower" />
+        <div className="text-over">
+          <p className="sale">Sale 50%</p>
+          <p className="discount">weekly discount —</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sale;
