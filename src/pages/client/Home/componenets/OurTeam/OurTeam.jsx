027151411e3d3c8c9c8f8527ca688754a6540afe
ilaha-jaf <ilash.jafarova@gmail.com>
2024-08-09 18:16:25 +0400

    new our team

--- src/pages/client/Home/componenets/OurTeam/OurTeam.jsx
+++ src/pages/client/Home/componenets/OurTeam/OurTeam.jsx
@@ -6,26 +6,26 @@ function OurTeam() {
     <div className='ourTeam'>
       <div className="container">
         <div className="row">
-          <p className="text-center">
+          <p className="text-center mb-4 color:green">
             Contacts</p>
-          <h3 className=" sectionTeam text-center"> Our Team</h3>
+          <h3 className=" sectionTeam text-center mb-5"> Our Team</h3>
         </div>
-        <div className='row'>
+        <div className="d-flex justify-content-center">
           <div className='col-md-4'>
             <img className='image-fluid' src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/portr1.jpg" alt="" />
-            <p className='text-center green-p'>Velva Kopf</p>
+            <p className='text-center green-p h4'>Velva Kopf</p>
             <p className='text-center green-p'>Biologist</p>
           </div>
           <div className='col-md-4'>
-            <img className='image-fluid' src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/portr1.jpg" alt="" />
-            <p className='text-center green-p'>Velva Kopf</p>
-            <p className='text-center green-p'>Biologist</p>
+            <img className='image-fluid' src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/portr2.jpg" alt="" />
+            <p className='text-center green-p h4'>Sarah Palmer</p>
+            <p className='text-center green-p'>Florist</p>
           </div>      
           
            <div className='col-md-4'>
-            <img className='image-fluid' src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/portr1.jpg" alt="" />
-            <p className='text-center green-p'>Velva Kopf</p>
-            <p className='text-center green-p'>Biologist</p>
+            <img className='image-fluid' src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/portr3.jpg" alt="" />
+            <p className='text-center green-p h4'>Jessica Swift</p>
+            <p className='text-center green-p '>Photographer</p>
           </div>
         </div>
         <div className=' section3 row '>
